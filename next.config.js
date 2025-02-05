ab57fc277e79f401ac525003a63bd8ca815a3d1a
MrGodOfNight <DenRussianBoy@mail.ru>
2025-02-05 20:19:22 +0500

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    experimental: {
+        webpackBuildWorker: true,
+        parallelServerBuildTraces: true,
+        parallelServerCompiles: true,
+    },
+};
 module.exports = config;
